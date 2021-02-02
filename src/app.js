b5ec6a031378114bb3d61c325096ee040bfb3767
Christopher Grady <grittygrady@gmail.com>
2021-02-02 14:25:13 -0800

    trying cors options

--- src/app.js
+++ src/app.js
@@ -12,9 +12,19 @@ const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common'
 
+app.set('trust proxy', 1);
+
+const inDev = config.NODE_ENV === 'development';
+
+app.use(cors({
+  origin: inDev ? 'http://localhost:3000' : ['https://vinyl-wishlist.vercel.app', 'https://vinyl-wishlist.herokuapp.com'],
+  credentials: true,
+  preflightContinue: false
+}))
+
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+
 
 app.use(reviewRouter)
 
