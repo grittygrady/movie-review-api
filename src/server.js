5a7faf32cf4496342e6697b97b338c7e3e1ce2af
Christopher Grady <grittygrady@gmail.com>
2021-02-03 11:00:10 -0800

    trying

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const { PORT } = require('./config');
 
 app.set('db', db);
 
+const pg = require('pg'); pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
+
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
 });
